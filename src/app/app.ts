eb58bfcbd5ac55da77ae0addd9e8f3d1e15c9558
Nicky Jones De Giorgi <stephennicholas.jones266@comunidadunir.net>
2025-12-11 12:16:50 +0100

    UPDATE 1: Subida de todos los archivos para realizar el control de versiones de la tarea.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductService, Product } from './services/product';
+import { ProductsList } from './components/products-list/products-list';
+import { ProductFormComponent } from './components/product-form/product-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductsList, ProductFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gestion-productos');
+
+  constructor (private productService: ProductService){
+    //this.productService.cargarProductos().subscribe(
+      //(datos :Product[]) => console.log('Productos cargados de la API: ', datos)
+    //)
+
+    this.productService.cargarProductos()
+  }
+
+  onProductoCreado(producto: any){
+    this.productService.agregarProductos(producto)
+  }
 }
---
+++ src/app/components/products-list/products-list.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter } from '@angular/core';
+import { ProductService, Product } from '../../services/product';
+import { ProductsCard } from '../products-card/products-card';
+
+@Component({
+  selector: 'app-products-list',
+  imports: [ProductsCard],
+  templateUrl: './products-list.html',
+  styleUrl: './products-list.css',
+})
+export class ProductsList {
+
+  productos: Product[] = []
+
+  constructor(private productService: ProductService) {
+    
+    this.productService.productos$.subscribe
+      (productos  => {
+        this.productos = productos;
+        console.log('Productos recibidos: ', productos)
+      } 
+    )
+  }
+
+  
+}
